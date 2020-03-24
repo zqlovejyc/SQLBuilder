4fa86918b571093f277d340e06be7770731062b4
zqlovejyc <943620963@qq.com>
2020-03-24 21:20:01 +0800

    新增Oracle DynamicParameters实现，用于支持Oracle游标类型；

--- SQLBuilder/Extensions.cs
+++ SQLBuilder/Extensions.cs
@@ -1094,4 +1094,56 @@ namespace SQLBuilder
         }
         #endregion
     }
+
+    /// <summary>
+    /// Oracle DynamicParameters实现，用于支持Oracle游标类型
+    /// </summary>
+    public class OracleDynamicParameters : SqlMapper.IDynamicParameters
+    {
+        private readonly DynamicParameters dynamicParameters = new DynamicParameters();
+
+        private readonly List<OracleParameter> oracleParameters = new List<OracleParameter>();
+
+        /// <summary>
+        /// Add
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="oracleDbType"></param>
+        /// <param name="direction"></param>
+        public void Add(string name, OracleDbType oracleDbType, ParameterDirection direction)
+        {
+            var oracleParameter = new OracleParameter(name, oracleDbType, direction);
+            oracleParameters.Add(oracleParameter);
+        }
+
+        /// <summary>
+        /// Add
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="oracleDbType"></param>
+        /// <param name="direction"></param>
+        /// <param name="value"></param>
+        /// <param name="size"></param>
+        public void Add(string name, OracleDbType oracleDbType, ParameterDirection direction, object value = null, int? size = null)
+        {
+            OracleParameter oracleParameter;
+            if (size.HasValue)
+                oracleParameter = new OracleParameter(name, oracleDbType, size.Value, value, direction);
+            else
+                oracleParameter = new OracleParameter(name, oracleDbType, value, direction);
+            oracleParameters.Add(oracleParameter);
+        }
+
+        /// <summary>
+        /// AddParameters
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="identity"></param>
+        public void AddParameters(IDbCommand command, SqlMapper.Identity identity)
+        {
+            ((SqlMapper.IDynamicParameters)dynamicParameters).AddParameters(command, identity);
+            if (command is OracleCommand oracleCommand)
+                oracleCommand.Parameters.AddRange(oracleParameters.ToArray());
+        }
+    }
 }
