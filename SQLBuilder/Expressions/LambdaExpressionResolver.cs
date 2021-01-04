68517fad5e51726956d8fd05d4dcf8672e125cb6
zqlovejyc <943620963@qq.com>
2021-01-04 11:45:06 +0800

    新增对Having语法的支持，及其对应单元测试用例；

--- SQLBuilder/Expressions/LambdaExpressionResolver.cs
+++ SQLBuilder/Expressions/LambdaExpressionResolver.cs
@@ -52,6 +52,19 @@ namespace SQLBuilder.Expressions
 
             return sqlWrapper;
         }
+
+        /// <summary>
+        /// Having
+        /// </summary>
+        /// <param name="expression">表达式树</param>
+        /// <param name="sqlWrapper">sql打包对象</param>
+        /// <returns>SqlWrapper</returns>
+        public override SqlWrapper Having(LambdaExpression expression, SqlWrapper sqlWrapper)
+        {
+            SqlExpressionProvider.Having(expression.Body, sqlWrapper);
+
+            return sqlWrapper;
+        }
         #endregion
     }
 }
