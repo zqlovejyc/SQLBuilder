b929aed1d8090010ce63e511626e5e32fda355e2
zqlovejyc <943620963@qq.com>
2021-06-23 11:35:28 +0800

    Table、Key、Column特性新增Format属性，用于某些与数据库关键字重名的表名、列名单独进行格式化；

---
+++ SQLBuilder.UnitTest/Models/TestTable.cs
@@ -0,0 +1,21 @@
+﻿using SQLBuilder.Attributes;
+
+namespace SQLBuilder.UnitTest
+{
+    [Table("Base_Test", Format = true)]
+    public class TestTable
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Column(Format = true)]
+        public string Group { get; set; }
+
+        [Column(Format = true)]
+        public string Order { get; set; }
+
+        public string OtherName { get; set; }
+    }
+}
